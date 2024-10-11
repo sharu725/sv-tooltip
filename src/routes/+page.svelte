3b8b7b5fc48a46658d095414d11ca3103d5687a2
sharath <sharath.dt@gmail.com>
2024-10-11 16:36:55 +0530

    udpate styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -21,7 +21,7 @@
 
     <div class="code">
       {@html Prism.highlight(
-        `<p>Something <Tooltip tip="It was actially a dream" top><b>wierd</b></Tooltip> happened last night</p>`,
+        `<p>Something <Tooltip tip="It was actially a dream" top>wierd</Tooltip> happened last night</p>`,
         Prism.languages[language]
       )}
     </div>
@@ -51,12 +51,6 @@
         >A button <br />can be taller <br />than normal <br />height
       </Button>
     </Tooltip>
-    <br />
-    <br />
-    <Tooltip tip="This is a button" left>
-      <Button>Component</Button>
-    </Tooltip>
-
     <br />
     <br />
     <div class="code">
@@ -141,34 +135,3 @@
     rel="stylesheet"
   />
 </svelte:head>
-
-<style>
-  .content {
-    max-width: 800px;
-    margin: 5rem auto;
-  }
-  fieldset {
-    margin-bottom: 2rem;
-  }
-  nav ul {
-    display: flex;
-    padding: 0;
-    list-style: none;
-  }
-  nav ul li {
-    margin-right: 1rem;
-  }
-
-  :global(.font-bold) {
-    font-weight: bold;
-  }
-  :global(.text-xl) {
-    font-size: 1.5em;
-  }
-  .code {
-    white-space: pre-wrap;
-    background-color: #eee;
-    padding: 10px;
-    border-radius: 4px;
-  }
-</style>
